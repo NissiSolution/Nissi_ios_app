429999e4cd01ed2ebb0da2b838e8033ae182d2fc
NissiSolution <incrediblenissitest@gmail.com>
2024-12-16 15:06:06 +0530

    version update

--- lib/views/location/map_history_activity.dart
+++ lib/views/location/map_history_activity.dart
@@ -1,11 +1,8 @@
 import 'dart:async';
-
-
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:http/http.dart';
 import 'package:intl/intl.dart';
-
 import '../../Supports/checkpassword.dart';
 import '../../Supports/classes.dart';
 import '../../Supports/constants.dart';
